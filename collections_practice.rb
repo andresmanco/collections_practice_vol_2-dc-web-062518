ae0aa83cbd30f4121ff82a79afa5e14721658500
Andres Manco <andresmanco@gmail.com>
2018-06-21 06:34:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,13 @@
 # your code goes here
 def begins_with_r(arr)
-  
+  n = 0
+  arr.each do |a|
+    if arr[0,1] == r
+      n+=1
+    end
+  end
+  if n == array.length
+    return true
+  else
+    false
 end
